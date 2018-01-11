4e233576f040f688aac154c1b47716f6652d60e9
David Lewis and Masen Matthews <david@>
2018-01-11 10:16:06 -0800

    Add images and if statement to show specified image

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  
+  var stomach = prompt("can you handle the madness?");
+  var stomach = stomach.toLowerCase();
+      if(stomach === "yes") {
+        $(".dragon").show();
+      } else if(stomach === "no") {
+        $(".unicorn").show();
+      } else {
+        alert("choose yes or no or meet your untimely demise");
+      }
 });
